39fd8646752e604e6b9c7352772ae646ea660413
mihastech <mihastechdeveloper@gmail.com>
2024-08-02 18:42:27 +0300

    changed

--- server/config/db.js
+++ server/config/db.js
@@ -119,6 +119,7 @@ const pool = new Pool({
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,  // Adjust as needed for your SSL configuration
   },
