58dfbf50065dbbf129083e67586fe4dc5c52cb1f
mihastech <mihastechdeveloper@gmail.com>
2024-08-05 10:52:15 +0300

    changed

--- client/my-react-app/src/axiosconfig.js
+++ client/my-react-app/src/axiosconfig.js
@@ -1,9 +1,18 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// const axiosBase = axios.create({
+//     // baseURL: 'http://localhost:5500/api/',
+//     // eslint-disable-next-line no-undef
+//     baseURL: process.env.REACT_APP_BACKEND_URL,
+// })
+
+// export default axiosBase
+
+import axios from 'axios';
 
 const axiosBase = axios.create({
-    // baseURL: 'http://localhost:5500/api/',
     // eslint-disable-next-line no-undef
     baseURL: process.env.REACT_APP_BACKEND_URL,
-})
+});
 
-export default axiosBase
+export default axiosBase;
