82244fe137d605d58f246cb1625df4537bb0c667
mihastech <mihastechdeveloper@gmail.com>
2024-08-08 15:28:49 +0300

    changed

--- client/my-react-app/src/pages/Home.jsx
+++ client/my-react-app/src/pages/Home.jsx
@@ -1380,6 +1380,12 @@ function Home() {
                     ) : (
                       <p>No profile image available</p>
                     )} */}
+
+{question.profileImage ? (
+          <img src={`${backendUrl}/${question.profileImage.replace(/\\/g, '/')}`} alt="Profile" className="profile-image" onError={(e) => { e.target.onerror = null; e.target.src = "fallback.jpg" }} />
+        ) : (
+          <p>No profile image available</p>
+        )}
                     <div className='username'>{question.username}</div>
                     <div className="question-container">
                       <input
